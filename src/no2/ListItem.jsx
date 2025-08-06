a2f3c0cd544a442ee356680ad7221ffc3d5e2e96
Cristian Yulma <cristianyulmaw@gmail.com>
2025-08-06 09:03:13 +0700

    Feat: Making list item component to make navbtn

---
+++ src/no2/ListItem.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+/**
+ * Digunakan untuk elemen list pada navbar
+ * @param {Object} props
+ * @param {string} props.to - Tujuan Navigasi dari Link
+ * @param {string} props.listText - Teks dari Link
+ */
+
+function ListItem(props) {
+  return (
+    <li className="cursor-pointer select-none">
+      <Link to={props.to}>{props.listText}</Link>
+    </li>
+  );
+}
+
+export default ListItem;
