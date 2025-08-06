42c2596df1de0eab1d95197adc0b246d2f4addc8
Cristian Yulma <cristianyulmaw@gmail.com>
2025-08-06 09:00:29 +0700

    Feat: Created for give the provider for authContext.js

---
+++ src/no2/AuthProvider.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import {AuthContext} from './authContext.js'
+
+export default function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  const login = (user) => {
+    setIsLoggedIn(true);
+    setUsername(user);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, username, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
