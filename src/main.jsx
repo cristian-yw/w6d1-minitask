8dae2c9d6908eba211cd5534e81ec52462f67d61
Cristian Yulma <cristianyulmaw@gmail.com>
2025-08-07 09:12:23 +0700

    Fix: Add redux provider to the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,18 @@ import ShoppingApp from './no1/ShoppingApp.jsx'
 import Router from './Router.jsx'
 import AuthProvider from './no2/AuthProvider';
 import TodoProvider from './no3/TodoProvider.jsx'
+import { Provider as ReduxProvider} from 'react-redux';
+import store from './redux/store.js';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
+   <ReduxProvider store={store}>
    <AuthProvider>
     <TodoProvider>
      <Router />
      </TodoProvider>
    </AuthProvider>
+   </ReduxProvider>
   </BrowserRouter>
 )
